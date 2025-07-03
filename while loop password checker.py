06757dfed88dda330878005561c87a475b9d566f
mayankshar-cyber <thedragonwarrior2001@gmail.com>
2025-07-03 21:09:53 +0530

    Add files via upload

---
+++ while loop password checker.py
@@ -0,0 +1,11 @@
+# password checker
+
+
+while True:
+    enter_pw=(input("enter password:"))
+    password="genius1"
+    if password!=enter_pw:
+        print("galat password hai sahi daalo")
+    else:
+        print("access granted moz karo")
+        break
